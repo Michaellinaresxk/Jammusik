03527b766d2f1e46d10915839a70aa383bd69840
marlonrodri7 <marlon7piri@gmail.com>
2024-03-28 11:07:19 -0500

    feature/creating the loginscreen

---
+++ src/presentation/components/shared/FormLogin.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Text, View, TextInput, StyleSheet } from "react-native";
+import { PrimaryButton } from "./PrimaryButton";
+export const Formlogin = () => {
+  return (
+    <View style={style.containerForm}>
+      <View style={style.form}>
+        <Text>Use name:</Text>
+        <TextInput placeholder="username" style={style.inputLogin} />
+        <Text>Username</Text>
+
+        <TextInput placeholder="password" style={style.inputLogin} />
+        <PrimaryButton label="SUBMIT" />
+
+      </View>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  containerForm: {
+    width: "100%",
+    height: "auto",
+    color: "white",
+  },
+
+  form: {},
+  inputLogin: {
+    border: "12px",
+    borderBottomColor: "black",
+  },
+});
--- src/presentation/screens/LoginScreen.tsx
+++ src/presentation/screens/LoginScreen.tsx
@@ -1,13 +1,50 @@
-import { View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import { PrimaryButton } from "../components/shared/PrimaryButton";
 import { PrimaryIcon } from "../components/shared/PrimaryIcon";
-import { globalStyles } from "../theme/Theme";
+import { globalColors, globalStyles } from "../theme/Theme";
+import { Formlogin } from "../components/shared/FormLogin";
 
 export const LoginScreen = () => {
+  const image = {
+    uri: "https://th.bing.com/th/id/OIP.4_mCu-7n0IvOe0y7dnNB-wHaE8?rs=1&pid=ImgDetMain",
+  };
+
   return (
-    <View style={globalStyles.container}>
-      <PrimaryIcon name="rocket-outline" size={30} color="#900" />
-      <PrimaryButton label="SUBMIT" />
-    </View>
+    <ImageBackground
+      source={image}
+      style={styles.containerImage}
+      resizeMode="cover"
+      alt="Imagen de fondo">
+      <View style={styles.containerLoginScreen}>
+        <View style={styles.containerLogo}>
+          <PrimaryIcon name="rocket-outline" size={200} color={globalColors.primary}/>
+        </View>
+        <View style={styles.containerForm}>
+          <Formlogin />
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  containerImage: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  containerLoginScreen: {
+    flex: 1,
+
+    backgroundColor: "black",
+  },
+  containerLogo: {
+    flex: 1,
+
+    backgroundColor: "blue",
+  },
+  containerForm: {
+    flex: 2,
+
+    backgroundColor: "black",
+  },
+});
