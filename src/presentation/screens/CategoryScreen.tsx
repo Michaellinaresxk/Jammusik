63ede8d13776b97d0f33d3b014dc2e675052f63a
Michael Linares <michaellinaresdbildungs@gmail.com>
2024-03-30 09:28:59 +0100

    Feature: Added navigation Route prop to the categoryCard, to send by clic to categoryScreen dinamic route.

--- src/presentation/screens/CategoryScreen.tsx
+++ src/presentation/screens/CategoryScreen.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { RootStackParamsList } from "../routes/StackNavigator";
 import { Text, View } from "react-native";
 
 export const CategoryScreen = () => {
+  const params =
+    useRoute<RouteProp<RootStackParamsList, "CategoryScreen">>().params;
+  const navigation = useNavigation();
+
   return (
     <View>
-      <Text>Category Screen</Text>
+      <Text> {params.title} </Text>
     </View>
   );
 };
