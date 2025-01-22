fa1b57877f86843fed90e35252780be8b9514f9c
Michael Linares <michaellinaresdbildungs@gmail.com>
2025-01-22 15:23:46 +0100

    fix typo in song Optional.

--- src/UI/components/shared/modals/SongOptionsModal.tsx
+++ src/UI/components/shared/modals/SongOptionsModal.tsx
@@ -23,7 +23,7 @@ type SongOptionsModalProps = {
   onToggleFavorite: () => void;
 };
 
-export const ForgotPasswordModalSongOptionsModal = ({
+export const SongOptionsModal = ({
   isVisible,
   onClose,
   onEdit,
