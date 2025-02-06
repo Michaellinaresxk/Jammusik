80f8a2b56c6f1ae8c81bbd90491b8b42b72ce99e
Michael Linares <michaellinaresdbildungs@gmail.com>
2025-02-06 14:07:35 +0100

    Removed utils folder.

--- src/utils/formatDuration.ts
+++
@@ -1,8 +0,0 @@
-export const formatDuration = ms => {
-  if (!ms) return '0:00';
-
-  const minutes = Math.floor(ms / 60000);
-  const seconds = Math.floor((ms % 60000) / 1000);
-
-  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
-};
